1015e413603f7a04e3acfaaa046f2190ed55b1dc
APLKLAT060761 <aplklat060761@APLKLAT060761.local>
2021-10-13 10:07:30 +0530

    Initial commit

---
+++ src/Listint.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Listint {
+    public static void main(String[] args){
+        ArrayList l=new ArrayList();
+        for(int i=1;i<=10;i++){
+            l.add(i);
+        }
+        Iterator itr=l.iterator();
+        while(itr.hasNext()){
+            Integer I=(Integer)itr.next();
+            System.out.println(I);
+        }
+    }
+}
